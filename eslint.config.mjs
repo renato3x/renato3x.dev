3b0b11991c34f8d5de86f60d5c2b6532f5199e48
renato3x <pereirarenato21@gmail.com>
2025-03-16 20:47:18 -0300

    chore: eslint configured

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +12,40 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    ignores: ['node_modules/', 'dist/'],
+  },
+  {
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      quotes: ['error', 'single', { avoidEscape: true }],
+      eqeqeq: ['error', 'always'],
+      semi: ['error', 'always'],
+      curly: ['error', 'all'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'prefer-const': 'error',
+      'no-var': 'error',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+      'eol-last': ['error', 'always'],
+      'no-trailing-spaces': 'error',
+      'no-template-curly-in-string': 'error',
+    },
+  },
 ];
 
 export default eslintConfig;
