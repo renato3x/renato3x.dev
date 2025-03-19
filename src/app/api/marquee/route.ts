b6a8ae5aa3aac65e7e43c47496fd5263a8f8a56d
renato3x <pereirarenato21@gmail.com>
2025-03-18 22:13:42 -0300

    feat: marque files api

---
+++ src/app/api/marquee/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server';
+import getConfig from 'next/config';
+import fs from 'node:fs';
+import path from 'node:path';
+
+export async function GET(): Promise<NextResponse<string[]>> {
+  const { serverRuntimeConfig } = getConfig();
+  const marqueeItemsPath = path.join(serverRuntimeConfig.PROJECT_ROOT, 'public', 'images', 'marquee');
+
+  const marqueeFiles = fs.readdirSync(marqueeItemsPath)
+    .map((filename) => `${process.env.APP_URL}/images/marquee/${filename}`);
+
+  return NextResponse.json<string[]>(marqueeFiles, { status: 200 });
+}
