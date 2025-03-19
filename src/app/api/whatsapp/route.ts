cad05966ba18610f3aa2923532442c1f930714a8
renato3x <pereirarenato21@gmail.com>
2025-03-18 21:32:08 -0300

    feat: fetching whatsapp number

---
+++ src/app/api/whatsapp/route.ts
@@ -0,0 +1,5 @@
+import { NextResponse } from "next/server";
+
+export async function GET(): Promise<Response> {
+  return new NextResponse('558892772736', { status: 200 });
+}
--- src/components/common/WhatsAppButton/index.tsx
+++ src/components/common/WhatsAppButton/index.tsx
@@ -1,4 +1,7 @@
-import Image from "next/image";
+'use client';
+
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
 import clsx from 'clsx';
 
 interface WhatsAppButtonProps {
@@ -6,11 +9,24 @@ interface WhatsAppButtonProps {
 }
 
 export default function WhatsAppButton({ highlight }: WhatsAppButtonProps) {
+  const [ whatsApp, setWhatsApp ] = useState<string>('');
+  const message = 'Olá, gostaria de solicitar um orçamento.';
+
+  useEffect(() => {
+    fetch('/api/whatsapp')
+    .then((response) => response.text())
+    .then((number) => setWhatsApp(number));
+  }, []);
+
   return (
-    <a className={clsx(
-      `flex justify-center items-center gap-1.5 px-[40] py-[14] rounded-[6px] hover:cursor-pointer`,
-      highlight ? 'bg-linear-to-r from-green-darker to-green-primary shadow-[0_11px_33px_0] shadow-green-shadow' : 'bg-green-secondary',
-    )}>
+    <a
+      href={`https://wa.me/${whatsApp}?text=${message}`}
+      target="_blank"
+      className={clsx(
+        `flex justify-center items-center gap-1.5 px-[40px] py-[14px] rounded-[6px] hover:cursor-pointer`,
+        highlight ? 'bg-linear-to-r from-green-darker to-green-primary shadow-[0_11px_33px_0] shadow-green-shadow' : 'bg-green-secondary',
+      )}
+    >
       <Image
         src="/images/wpp-icon.svg"
         alt="WhatsApp Icon"
