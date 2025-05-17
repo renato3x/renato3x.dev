5ee7da7faa4f97f18a3531b955d71328e84cb53d
renato3x <pereirarenato21@gmail.com>
2025-05-16 22:51:50 -0300

    fix: use other approach to get marquee images

--- env.d.ts
+++ env.d.ts
@@ -5,6 +5,7 @@ declare namespace NodeJS {
     NEXT_PUBLIC_FORMATTED_WHATSAPP_NUMBER: string;
     NEXT_PUBLIC_CONTACT_EMAIL: string;
     NEXT_PUBLIC_APPLICATION_URL: string;
+    NEXT_PUBLIC_MARQUEE_LENGTH: `${number}`;
     NODE_ENV: 'production' | 'development' | 'test'
   }
 }
