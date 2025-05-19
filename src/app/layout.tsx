f5dd8805530a9a27113209add88fc07b729f089f
renato3x <pereirarenato21@gmail.com>
2025-05-19 18:12:43 -0300

    refactor: remove meta pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Archivo, Chakra_Petch } from 'next/font/google';
-import Image from 'next/image';
 import Script from 'next/script';
 
 const archivo = Archivo({
@@ -168,24 +167,6 @@ export default function RootLayout({
     <html lang="pt-br" className={`${archivo.variable} ${chakra.variable} scroll-smooth`}>
       <head>
         <Script
-          id="facebook-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1002010778754177');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
-        <Script
           id="google-tag-manager"
           dangerouslySetInnerHTML={{
             __html: `
@@ -199,15 +180,6 @@ export default function RootLayout({
         />
       </head>
       <body className="bg-black-primary text-white-primary">
-        <noscript>
-          <Image
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=1002010778754177&ev=PageView&noscript=1"
-            alt=""
-          />
-        </noscript>
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-5ZF8RB98"
