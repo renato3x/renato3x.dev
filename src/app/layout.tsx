a129812500c566417309976b9d61d37e2ed00958
renato3x <pereirarenato21@gmail.com>
2025-05-19 18:10:08 -0300

    feat: use gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -185,6 +185,18 @@ export default function RootLayout({
             `,
           }}
         />
+        <Script
+          id="google-tag-manager"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5ZF8RB98');
+            `,
+          }}
+        />
       </head>
       <body className="bg-black-primary text-white-primary">
         <noscript>
@@ -194,6 +206,14 @@ export default function RootLayout({
             style={{ display: 'none' }}
             src="https://www.facebook.com/tr?id=1002010778754177&ev=PageView&noscript=1"
             alt=""
+          />
+        </noscript>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5ZF8RB98"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
           />
         </noscript>
         {children}
